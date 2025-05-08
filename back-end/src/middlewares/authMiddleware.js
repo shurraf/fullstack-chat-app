2eca634e21a4dc8b8f833ff997ee8cbc06fd33cc
shurraf <rrafshughran@gmail.com>
2025-05-08 18:19:24 +0600

    Update authMiddleware.js

--- back-end/src/middlewares/authMiddleware.js
+++ back-end/src/middlewares/authMiddleware.js
@@ -1,30 +1,29 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
 
-exports.protectRoute = async(req,res,next) => {
-  try{
-    const token = req.cookies.jwt
-    if(!token){
-      return res.status(401).json({message: 'please login or register first'})
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ message: 'please login or register first' });
     }
 
-    const decoded = jwt.verify(token,process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if(!decoded){
-      return res.status(401).json({ message: "unauthorized user" });
+    if (!decoded) {
+      return res.status(401).json({ message: 'unauthorized user' });
     }
 
-    const user = await User.findById(decoded.userId).select('-password')
+    const user = await User.findById(decoded.userId).select('-password');
 
-    if(!user){
-      return res.status(404).json({ message: "user not found" });
+    if (!user) {
+      return res.status(404).json({ message: 'user not found' });
     }
 
-    req.user = user
-    next()
-
-  }catch(error){
-    console.log('error in protectRoute middleware ',error.message)
-    return res.status(500).json({ message: "internal server error" });
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log('error in protectRoute middleware ', error.message);
+    return res.status(500).json({ message: 'internal server error' });
   }
-}
+};
