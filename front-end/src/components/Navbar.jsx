514cde268996a253d51919088d7877fe572696f8
shurraf <rrafshughran@gmail.com>
2025-05-24 01:13:27 +0600

    Update Navbar.jsx

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
               <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary"></MessageSquare>
               </div>
-              <h1 className="text-lg font-bold">Lime Chat</h1>
+              <h1 className="text-lg font-bold">Quick Chat</h1>
             </Link>
           </div>
 
